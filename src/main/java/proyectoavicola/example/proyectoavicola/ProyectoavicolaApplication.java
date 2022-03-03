6f05de6bb505640cd686c689f86fefb001144fe2
rosauracotos <rosacotossullon@gmail.com>
2022-03-03 18:40:37 -0500

    primer avance

---
+++ src/main/java/proyectoavicola/example/proyectoavicola/ProyectoavicolaApplication.java
@@ -0,0 +1,13 @@
+package proyectoavicola.example.proyectoavicola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoavicolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoavicolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/proyectoavicola/example/proyectoavicola/ProyectoavicolaApplicationTests.java
@@ -0,0 +1,13 @@
+package proyectoavicola.example.proyectoavicola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoavicolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
